494f1db9a4eabc099725d04a9b39c8bfa8cee6e2
joadsonteixeira <joadsonchagas@aluno.uema.br>
2023-11-26 01:13:54 -0300

    Add social

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col justify-between relative p-2 lg:p-24">
+    <main className="flex min-h-screen flex-col justify-center lg:justify-between relative p-2 lg:p-24">
       <div className="z-20  w-full items-center justify-between font-mono text-sm lg:flex right-0">
         <p className="fixed left-0 top-0 flex w-full text-2xl justify-center from-zinc-200 pb-6 pt-8 backdrop-blur-2xl lg:text-3xl lg:static lg:w-auto  lg:p-4 text-logo">
           &lt;/&gt; Joadson Teixeira
@@ -20,24 +20,44 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="flex justify-center mb-64 lg:justify-end">
-        <div className="console-container z-10 h-64 w-4/5 lg:h-96 lg:w-2/5">
-          <div className="console-top-bar justify-between relative p-1">
-            <div className="flex justify-end">
-              <div className="console-top-btn ml-1"></div>
-              <div className="console-top-btn ml-1"></div>
-              <div className="console-top-btn ml-1"></div>
+      <div className="flex mb-64 justify-center h-full lg:justify-end">
+        <div className="flex h-64 w-4/5 lg:h-96 lg:w-2/5">
+          <div className="flex flex-col items-center z-10 w-full">
+            <div className="console-container h-full w-full">
+              <div className="console-top-bar justify-between relative p-1">
+                <div className="flex justify-end">
+                  <div className="console-top-btn ml-1"></div>
+                  <div className="console-top-btn ml-1"></div>
+                  <div className="console-top-btn ml-1"></div>
+                </div>
+              </div>
+              <TypingAnimation
+                textContent={
+                  ">Este site está em desenvolvimento.\n>Novidades em breve :)\n>"
+                }
+              />
+            </div>
+
+            <div className="relative flex flex-row mt-3 items-center">
+              <div className="social-container flex">
+                <a
+                  href="https://www.linkedin.com/in/joadsonteixeira/"
+                  target="_blank"
+                >
+                  <div className="social-btn-icon w-8 h-auto shadow-md rounded shadow-neutral-900">
+                    <img src="linkedin-logo.png" alt="Linkedin" />
+                  </div>
+                </a>
+                <a href="https://instagram.com/joadsoncastro/" target="_blank">
+                  <div className="social-btn-icon w-8 h-auto ml-3 shadow-md rounded-md shadow-neutral-900">
+                    <img src="instagram-logo.png" alt="" />
+                  </div>
+                </a>
+              </div>
             </div>
           </div>
-          <TypingAnimation
-            textContent={
-              ">Este site está em desenvolvimento.\n>Novidades em breve :)\n>"
-            }
-          />
         </div>
       </div>
-
-      <div className="footer"></div>
 
       <div className="bg-image absolute lg:w-1/2 bottom-0 left-0 lg:pl-24">
         <img src="programmer.png" alt="" />
